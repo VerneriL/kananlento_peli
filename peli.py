90e9e7a77ca2097111d41d5534cf02757b6f1047
Verneri <verneri.lahteenoja@gmail.com>
2023-03-27 18:30:22 +0300

    working background images and logic

--- peli.py
+++ peli.py
@@ -10,11 +10,34 @@ class Game():
         self.screen = pygame.display.set_mode((800, 600))
         self.running = False
         self.init_graphics()
+        self.init_objects()
 
     def init_graphics(self):
-        img_bird1 = pygame.image.load("images/chicken/flying/frame-1.png")
-        self.img_bird1 = pygame.transform.rotozoom(img_bird1, 0, 1/16)
+        self.bird_frame = 0
+        bird_imgs = [
+            pygame.image.load(f"images/chicken/flying/frame-{i}.png")
+            for i in [1, 2, 3, 4]
+        ]
+        self.bird_imgs = [
+            pygame.transform.rotozoom(x, 0, 1/16)
+            for x in bird_imgs
+        ]
+        bg_imgs = [
+            pygame.image.load(f"images/background/layer_{i}.png")
+            for i in [1, 2, 3]
+        ]
+        self.bg_imgs = [
+            pygame.transform.rotozoom(x, 0, 600 / x.get_height()).convert_alpha()
+            for x in bg_imgs
+        ]
 
+    def init_objects(self):
+        self.bird_y_speed = 0
+        self.bird_pos = (200, 000)
+        self.bird_lift = False
+        self.bg0_pos = 0
+        self.bg1_pos = 0
+        self.bg2_pos = 0
 
     def run(self):
         clock = pygame.time.Clock()
@@ -23,23 +46,61 @@ class Game():
             self.handle_events()
             self.handle_game_logic()
             self.update_screen()
-    
             clock.tick(60) # Wait until refresh rate is 60fps
         pygame.quit()
 
     def handle_events(self):
-        for event in pygame.event.get(): # Loop through events
+        # Loop through events
+        for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
                 self.running = False
-        
+            elif event.type == pygame.KEYDOWN:
+                if event.key in (pygame.K_SPACE, pygame.K_UP):
+                    self.bird_lift = True
+            elif event.type == pygame.KEYUP:
+                if event.key in (pygame.K_SPACE, pygame.K_UP):
+                    self.bird_lift = False
 
     def handle_game_logic(self):
-        pass
+        # Background speeds
+        self.bg0_pos -= 0.25
+        self.bg1_pos -= 0.5
+        self.bg2_pos -= 2
+
+        # Bird y coordinate
+        bird_y = self.bird_pos[1] 
+        
+        if self.bird_lift:
+            # Lift bird y-pixel per frame
+            self.bird_y_speed -= 0.5
+            # Acceleration
+            self.bird_frame += 1
+        else:
+            # Gravity
+            self.bird_y_speed += 0.2
+
+        # Move bird based on bird speed
+        bird_y += self.bird_y_speed
+        self.bird_pos = (self.bird_pos[0], bird_y)
 
     def update_screen(self):    
-        self.screen.fill((230, 230, 255)) # Light blue
+        # self.screen.fill((230, 230, 255)) # Light blue
+        # Background display
+        self.screen.blit(self.bg_imgs[0], (self.bg0_pos, 0))
+        self.screen.blit(self.bg_imgs[1], (self.bg1_pos, 0))
+        self.screen.blit(self.bg_imgs[2], (self.bg2_pos, 0))
+
+        # Determine bird angle
+        angle = -90 * 0.08 * self.bird_y_speed
+        angle = max(min(angle, 60), -60)
+
+        # Draw bird from images depending on frames up to 3
+        bird_img_i = self.bird_imgs[(self.bird_frame // 3) % 4]
         # Draw bird
-        self.screen.blit(self.img_bird1, (100, 100))
+        bird_img = pygame.transform.rotozoom(bird_img_i, angle, 1)
+        self.screen.blit(bird_img, self.bird_pos)
+
+        # Display
         pygame.display.flip()
 
 
