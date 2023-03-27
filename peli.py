f0c7773ffe7bbfaf9d7506bda18be1118329ca23
Verneri <verneri.lahteenoja@gmail.com>
2023-03-27 13:20:34 +0300

    fixed indentation

--- peli.py
+++ peli.py
@@ -8,6 +8,13 @@ class Game():
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
+        self.running = False
+        self.init_graphics()
+
+    def init_graphics(self):
+        img_bird1 = pygame.image.load("images/chicken/flying/frame-1.png")
+        self.img_bird1 = pygame.transform.rotozoom(img_bird1, 0, 1/16)
+
 
     def run(self):
         clock = pygame.time.Clock()
@@ -20,18 +27,20 @@ class Game():
             clock.tick(60) # Wait until refresh rate is 60fps
         pygame.quit()
 
-def handle_events(self):
-    for event in pygame.event.get(): # Loop through events
-        if event.type == pygame.QUIT:
-            self.running = False
-    
+    def handle_events(self):
+        for event in pygame.event.get(): # Loop through events
+            if event.type == pygame.QUIT:
+                self.running = False
+        
 
-def handle_game_logic(self):
-    pass
+    def handle_game_logic(self):
+        pass
 
-def update_screen(self):    
-    self.screen.fill("purple")
-    pygame.display.flip()
+    def update_screen(self):    
+        self.screen.fill((230, 230, 255)) # Light blue
+        # Draw bird
+        self.screen.blit(self.img_bird1, (100, 100))
+        pygame.display.flip()
 
 
 if __name__=="__main__":
